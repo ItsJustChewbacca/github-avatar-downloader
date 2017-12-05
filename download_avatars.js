cd1cbfa25751714defca5cd4055f3aa7a6104a49
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-12-05 02:21:18 +0000

    added function to get repo contributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,9 @@
 var request = require("request");
 console.log("welcome to the github avatar downloader");
+
+function getRepoContributers(repoOwner, repoName, cb) {}
+
+getRepoContributers("jquery", "jquery", function(err, result) {
+  console.log("Get absolutely wrecked by", err);
+  console.log("Mad hacks", result);
+});
