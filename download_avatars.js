805127e191756cebe6299f5d800b05e714dba049
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-12-05 04:54:06 +0000

    fetched all the URIs and sent them tto a new file path with the login as the file name

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,22 @@
 var request = require("request");
 var secrets = require("./secrets.js");
+var fs = require("fs");
 console.log("welcome to the github avatar downloader");
+//I need to get each image url
+//I need to save the image to a filepath
+function downloadImageByURL(url, filepath) {
+  request
+    .get(url)
+    .on("error", function(error) {
+      console.log("get wrecked:", error);
+    })
+    .on("response", function(response) {
+      console.log("ResponseStatusCode:", response.statusCode);
+      console.log("ResponseStatusMessage:", response.statusMessage);
+      console.log("ResponseContentType:", response.headers);
+    })
+    .pipe(fs.createWriteStream(filepath));
+}
 
 function getRepoContributors(repoOwner, repoName, callback) {
   var options = {
@@ -20,6 +36,7 @@ function getRepoContributors(repoOwner, repoName, callback) {
       error,
       JSON.parse(body).forEach(function(key) {
         console.log(key.avatar_url);
+        downloadImageByURL(key.avatar_url, "./avatars/" + key.login + ".jpg");
       })
     );
   });
