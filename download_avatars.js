f1c3446a61c688ec4cf33ccfb3920612b03bc43b
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-12-05 03:30:46 +0000

    required ghost file to project and parsed data into Json string

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,29 @@
 var request = require("request");
+var secrets = require("./secrets.js");
 console.log("welcome to the github avatar downloader");
 
-function getRepoContributers(repoOwner, repoName, cb) {}
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    url:
+      "https://api.github.com/repos/" +
+      repoOwner +
+      "/" +
+      repoName +
+      "/contributors",
+    headers: {
+      "user-agent": "request",
+      Authentication: secrets
+    }
+  };
+  request(options, function(error, response, body) {
+    callback(error, JSON.parse(body));
+  });
+}
 
-getRepoContributers("jquery", "jquery", function(err, result) {
-  console.log("Get absolutely wrecked by", err);
-  console.log("Mad hacks", result);
+getRepoContributors("jquery", "jquery", function(error, result) {
+  if (error) {
+    console.log("Get absolutely wrecked by", error);
+    return;
+  }
+  console.log("Mad hacks:", result);
 });
